256295c54667caae69f3a49d4de0a908fa73a8e7
Marko Turchyn <marko.turchyn@gmail.com>
2026-03-18 22:49:15 -0400

    refactoring, add ability to load phantoms from file

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// Image Size N x N
 const int N = 180;
 const int HALF = (N - 1) / 2;
 
@@ -12,24 +13,44 @@ const short COLOR_GREY = 200;
 const short COLOR_GREY_LIGHT = 150;
 const short COLOR_WHITE = 255;
 
-void normalizeImageColors(int sinogram[][N]);
-
+// Function prototypes
+void loadImage(short pixels[][N], string);
 void rotateImage(short pixels[][N], short newPixels[][N], double angle);
 void clearImage(short pixels[][N]);
-
-void fillRectangle(short pixels[][N], int row0, int col0, int size,
-                   int x1, int y1, int x2, int y2);
-void fillCircle(short pixels[][N], int row0, int col0, int size,
-                int xc, int yc, int R);
+void normalizeImageColors(int sinogram[][N]);
+void processImageWithPhantom(short Pixels[][N]);
 
 int main() {
   short Pixels[N][N];
-  int sinogram[N][N] = {};
 
+  // Just nullifying the image array before loading new image
+  clearImage(Pixels);
+  
+  /**
+   * Processing phantom_01
+   */
+  loadImage(Pixels, "./data/shapesPhantom.txt");
+  Display image01Pixels(Pixels, "./output/Phantom_01_pixels");
+  processImageWithPhantom(Pixels);
+  Display image01Sinogram(Pixels, "./output/Phantom_01_sinogram");
+
+
+  // Clean up
   clearImage(Pixels);
 
-  fillCircle(Pixels, 0, 0, HALF, 45, 45, 15);
-  fillRectangle(Pixels, HALF, HALF, HALF, 15, 15, 62, 62);
+  /**
+   * Processing phantom_02
+   */
+  loadImage(Pixels, "./data/headPhantom.txt");
+  Display image02Pixels(Pixels, "./output/Phantom_02_pixels");
+  processImageWithPhantom(Pixels);
+  Display image02Sinogram(Pixels, "./output/Phantom_02_sinogram");
+
+  return 0;
+}
+
+void processImageWithPhantom(short Pixels[][N]) {
+  int sinogram[N][N] = {};
 
   for (int i = 0; i < N; i++) {
     short newPixels[N][N];
@@ -46,43 +67,15 @@ int main() {
 
   normalizeImageColors(sinogram);
 
-  short sinogramShort[N][N];
   for (int r = 0; r < N; r++)
     for (int c = 0; c < N; c++)
-      sinogramShort[r][c] = (short)sinogram[r][c];
-
-  Display imagePixels(Pixels, "./output/output-pixels");
-  Display imageSinogram(sinogramShort, "./output/output-sinogram");
-  return 0;
+      Pixels[r][c] = (short)sinogram[r][c];
 }
 
 void clearImage(short pixels[][N]) {
   for (int row = 0; row < N; row++)
     for (int col = 0; col < N; col++)
       pixels[row][col] = 0;
-}
-
-void fillRectangle(short pixels[][N], int row0, int col0, int size,
-                   int x1, int y1, int x2, int y2) {
-  // (x-x1)(x-x2) <= 0  and  (y-y1)(y-y2) <= 0
-  for (int row = row0; row < row0 + size; row++)
-    for (int col = col0; col < col0 + size; col++) {
-      int x = col - col0, y = row - row0;
-      if ((x - x1) * (x - x2) <= 0 && (y - y1) * (y - y2) <= 0)
-        pixels[row][col] = COLOR_WHITE;
-    }
-}
-
-void fillCircle(short pixels[][N], int row0, int col0, int size,
-                int xc, int yc, int R) {
-  // (x-xc)^2 + (y-yc)^2 <= R^2
-  for (int row = row0; row < row0 + size; row++)
-    for (int col = col0; col < col0 + size; col++) {
-      int x = col - col0, y = row - row0;
-      int dx = x - xc, dy = y - yc;
-      if (dx * dx + dy * dy <= R * R)
-        pixels[row][col] = COLOR_WHITE;
-    }
 }
 
 void normalizeImageColors(int sinogram[][N]) {
@@ -121,4 +114,14 @@ void rotateImage(short pixels[][N], short newPixels[][N], double angle) {
       }
     }
   }
+}
+
+void loadImage(short pixels[][N], string fileName) {
+    ifstream file;
+    file.open(fileName);
+    if (!file) cout << "File does not exist!";
+    for (int row = 0; row < N; row++)
+        for (int col = 0; col < N; col++)
+            file >> pixels[row][col];
+    file.close();
 }
