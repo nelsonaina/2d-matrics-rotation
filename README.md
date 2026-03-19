908108c2f68d46a30196bd83e1bff94166aee041
Marko Turchyn <marko.turchyn@gmail.com>
2026-03-18 22:19:16 -0400

    add normalization, fix rotation

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
+# BMCC Research - 2d matrics rotation and sinogram
 
 ### Run compilation and exec the binary
 
+For MacOS
 ```sh
-$ g++ main.cpp && ./a.out
+g++ main.cpp -o ./bin/2drt && ./bin/2drt
 ```
 
--- main.cpp
+++ main.cpp
@@ -1,11 +1,60 @@
 #include <iostream>
+#include <cmath>
 #include "Display.h"
+
 using namespace std;
 
 const int N = 180;
-const int HALF = N / 2;
+const int HALF = (N - 1) / 2;
+
+// Colors
 const short COLOR_GREY = 200;
 const short COLOR_GREY_LIGHT = 150;
+const short COLOR_WHITE = 255;
+
+void normalizeImageColors(int sinogram[][N]);
+
+void rotateImage(short pixels[][N], short newPixels[][N], double angle);
+void clearImage(short pixels[][N]);
+
+void fillRectangle(short pixels[][N], int row0, int col0, int size,
+                   int x1, int y1, int x2, int y2);
+void fillCircle(short pixels[][N], int row0, int col0, int size,
+                int xc, int yc, int R);
+
+int main() {
+  short Pixels[N][N];
+  int sinogram[N][N] = {};
+
+  clearImage(Pixels);
+
+  fillCircle(Pixels, 0, 0, HALF, 45, 45, 15);
+  fillRectangle(Pixels, HALF, HALF, HALF, 15, 15, 62, 62);
+
+  for (int i = 0; i < N; i++) {
+    short newPixels[N][N];
+
+    clearImage(newPixels);
+    rotateImage(Pixels, newPixels, (-1) * i);
+
+    for (int row = 0; row < N; row++) {
+      for (int col = 0; col < N; col++) {
+        sinogram[i][col] += newPixels[row][col];
+      }
+    }
+  }
+
+  normalizeImageColors(sinogram);
+
+  short sinogramShort[N][N];
+  for (int r = 0; r < N; r++)
+    for (int c = 0; c < N; c++)
+      sinogramShort[r][c] = (short)sinogram[r][c];
+
+  Display imagePixels(Pixels, "./output/output-pixels");
+  Display imageSinogram(sinogramShort, "./output/output-sinogram");
+  return 0;
+}
 
 void clearImage(short pixels[][N]) {
   for (int row = 0; row < N; row++)
@@ -20,7 +69,7 @@ void fillRectangle(short pixels[][N], int row0, int col0, int size,
     for (int col = col0; col < col0 + size; col++) {
       int x = col - col0, y = row - row0;
       if ((x - x1) * (x - x2) <= 0 && (y - y1) * (y - y2) <= 0)
-        pixels[row][col] = COLOR_GREY;
+        pixels[row][col] = COLOR_WHITE;
     }
 }
 
@@ -32,44 +81,44 @@ void fillCircle(short pixels[][N], int row0, int col0, int size,
       int x = col - col0, y = row - row0;
       int dx = x - xc, dy = y - yc;
       if (dx * dx + dy * dy <= R * R)
-        pixels[row][col] = COLOR_GREY_LIGHT;
+        pixels[row][col] = COLOR_WHITE;
     }
+}
+
+void normalizeImageColors(int sinogram[][N]) {
+  // Find max value for normalization to 0-255
+  int maxVal = 0;
+  for (int row = 0; row < N; row++)
+    for (int col = 0; col < N; col++)
+      if (sinogram[row][col] > maxVal) maxVal = sinogram[row][col];
+
+  for (int row = 0; row < N; row++)
+    for (int col = 0; col < N; col++)
+      sinogram[row][col] = sinogram[row][col] * 255 / maxVal;
 }
 
 /**
  * URL = https://www.geeksforgeeks.org/maths/rotation-matrix/
+ * Section of page = 2D Rotation Matrix
  */
-void rotateImage(short pixels[][N], short newPixels[][N], double degrees) {
-    double cos_a = cos(degrees);
-    double sin_a = sin(degrees);
-    
-    for (int row = 0; row < N; row++) {
-        for (int col = 0; col < N; col++) {
-            int x = col - HALF;
-            int y = row - HALF;
-            
-            int newCol = (int)(x * cos_a - y * sin_a) + HALF;
-            int newRow = (int)(x * sin_a + y * cos_a) + HALF;
-            
-            if (newRow >= 0 && newRow < N && newCol >= 0 && newCol < N) {
-                newPixels[newRow][newCol] = pixels[row][col];
-            }
-        }
+void rotateImage(short pixels[][N], short newPixels[][N], double angle) {
+  double delta = angle * M_PI / 180.0;
+
+  // Inverse mapping: for each destination pixel, find source pixel
+  for(int i = 0; i < N; i++) {
+    for(int j = 0; j < N; j++) {
+      double x = j - HALF;
+      double y = i - HALF;
+
+      double srcX = x * cos(delta) + y * sin(delta);
+      double srcY = -x * sin(delta) + y * cos(delta);
+
+      int sj = round(srcX + HALF);
+      int si = round(srcY + HALF);
+
+      if(sj >= 0 && sj < N && si >= 0 && si < N) {
+        newPixels[i][j] = pixels[si][sj];
+      }
     }
-}
-
-int main() {
-  short Pixels[N][N];
-  short newPixels[N][N
-
-  clearImage(Pixels);
-  clearImage(newPixels);
-
-  fillRectangle(Pixels, 0, 0, HALF, 10, 10, 80, 80);
-  fillCircle(Pixels, HALF, HALF, HALF, 45, 45, 40);
-
-  rotateImage(Pixels, newPixels, 90.0 * M_PI / 180.0);
-
-  Display image(newPixels, "output");
-  return 0;
+  }
 }
